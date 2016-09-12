6be1a51c7bbd36ba4d1f36c0793319a3fbd0c4f7
aceo <105021035@live.asia.edu.tw>
2016-09-12 15:50:55 +0800

    homework
    12345

---
+++ asia105021035/src/a12345/asia12345.java
@@ -0,0 +1,14 @@
+package a12345;
+
+public class asia12345 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            System.out.println("1212121212121");
+            
+	}
+
+}
