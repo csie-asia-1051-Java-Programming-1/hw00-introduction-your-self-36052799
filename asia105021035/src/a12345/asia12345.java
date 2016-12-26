cd89413c81f818d39b3bb11e24a002b581febffa
36052799 <105021035@live.asia.edu.tw>
2016-12-26 14:22:23 +0800

    Update asia12345.java

--- asia105021035/src/a12345/asia12345.java
+++ asia105021035/src/a12345/asia12345.java
@@ -7,7 +7,9 @@ public class asia12345 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-            System.out.println("1212121212121");
+            System.out.println("我是王文宏");
+            System.out.println("資工系1A");
+            System.out.println("學號105021035");
             
 	}
 
